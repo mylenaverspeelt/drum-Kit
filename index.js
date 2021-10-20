cf86336221c80e168db8069829d6a081f0c2b399
Mylena <mylena49@hotmail.com>
2021-10-20 19:13:00 -0300

    fisrt

---
+++ index.js
@@ -0,0 +1,12 @@
+document.querySelector(".w").addEventListener('click', clicando)
+document.querySelector(".a").addEventListener('click', clicando)
+document.querySelector(".s").addEventListener('click', clicando)
+document.querySelector(".d").addEventListener('click', clicando)
+document.querySelector(".j").addEventListener('click', clicando)
+document.querySelector(".k").addEventListener('click', clicando)
+document.querySelector(".l").addEventListener('click', clicando)
+
+function clicando(){
+alert('oi');
+}
+

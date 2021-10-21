f47eea2911c74722b644d37dc9d44a78b5452947
Mylena Verspeelt <mylena49@hotmail.com>
2021-10-21 11:36:40 -0300

    update

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-document.querySelector(".w").addEventListener('click', clicando)
-document.querySelector(".a").addEventListener('click', clicando)
-document.querySelector(".s").addEventListener('click', clicando)
-document.querySelector(".d").addEventListener('click', clicando)
-document.querySelector(".j").addEventListener('click', clicando)
-document.querySelector(".k").addEventListener('click', clicando)
-document.querySelector(".l").addEventListener('click', clicando)
-
-function clicando(){
-alert('oi');
-}
-
-
-//////////////////////////////
-
 var qtdeBotoes = document.querySelectorAll('.drum').length;
 
-for (var i=0; i<qtdeBotoes; i++){
-    document.querySelectorAll('.drum')[i].addEventListener('click', function(){
-        alert('oi');
-    });
+ for (var i=0; i<qtdeBotoes; i++){
+  document.querySelectorAll('.drum')[i].addEventListener('click', function(){
+       var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+   });
 }
 
-
-//////////////
-
-document.querySelectorAll(".drum").addEventListener('click', clicando);
-
-function clicando(){
-alert('oi');
-}

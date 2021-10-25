6a414a78a8eb48dad94b3f42c406655671c15f32
Mylena Verspeelt <mylena49@hotmail.com>
2021-10-25 09:44:42 -0300

    finalversion

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//DETECTAR BOTAO PRESSIONADO
+
 var qtdeBotoes = document.querySelectorAll('.drum').length;
 
 for (var i = 0; i < qtdeBotoes; i++) {
@@ -5,39 +7,66 @@ for (var i = 0; i < qtdeBotoes; i++) {
 
       var botaoInnerHTML = this.innerHTML;
 
-      switch (botaoInnerHTML) {
-
-         case "w":
-            var audio1 = new Audio("sounds/snare.mp3");
-            audio1.play();
-            break;
-         case "a":
-            var audio2 = new Audio("sounds/tom-4.mp3");
-            audio2.play();
-            break;
-         case "s":
-            var audio3 = new Audio("sounds/crash.mp3");
-            audio3.play();
-            break;
-         case "d":
-            var audio4 = new Audio("sounds/tom-1.mp3");
-            audio4.play();
-            break;
-         case "j":
-            var audio5 = new Audio("sounds/kick-bass.mp3");
-            audio5.play();
-            break;
-         case "k":
-            var audio6 = new Audio("sounds/tom-3.mp3");
-            audio6.play();
-            break;
-         case "l":
-            var audio7 = new Audio("sounds/tom-2.mp3");
-            audio7.play();
-            break;
-
-         default: console.log(botaoInnerHTML);
-            break;
-      }
+      tocarSom(botaoInnerHTML);
+      animacao(botaoInnerHTML);
    });
+}
+
+//DETECTAR TECLA PRESSIONADA
+
+document.addEventListener('keydown', function (event) {
+   tocarSom(event.key);
+   animacao(event.key);
+});
+
+//FUNÇÃO QUE RECEBE O PARAMETRO SEJA DO BOTAO SEJA DA TECLA PRESSIONADA PRA TOCAR O SOM 
+
+function tocarSom(key) {
+   switch (key) {
+
+      case "w":
+         var audio1 = new Audio("sounds/snare.mp3");
+         audio1.play();
+         break;
+      case "a":
+         var audio2 = new Audio("sounds/tom-4.mp3");
+         audio2.play();
+         break;
+      case "s":
+         var audio3 = new Audio("sounds/crash.mp3");
+         audio3.play();
+         break;
+      case "d":
+         var audio4 = new Audio("sounds/tom-1.mp3");
+         audio4.play();
+         break;
+      case "j":
+         var audio5 = new Audio("sounds/kick-bass.mp3");
+         audio5.play();
+         break;
+      case "k":
+         var audio6 = new Audio("sounds/tom-3.mp3");
+         audio6.play();
+         break;
+      case "l":
+         var audio7 = new Audio("sounds/tom-2.mp3");
+         audio7.play();
+         break;
+
+      default: console.log(botaoInnerHTML);
+         break;
+   }
+}
+
+// FUNÇÃO PRA FAZER ANIMAÇÃO 
+
+function animacao(key) {
+   var botaoAtivo = document.querySelector("." + key);
+
+   botaoAtivo.classList.add("pressed");
+
+   setTimeout(function () {
+      botaoAtivo.classList.remove("pressed");
+   }, 100);
+
 }
